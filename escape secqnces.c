d92ea26fb2f17deecac8392999dadcbd44981cc3
ratna05-star <rojaratna2007@gmail.com>
2026-02-13 12:19:59 +0530

    Create escape secqnces.c

---
+++ escape secqnces.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // TODO: Print header with tab escape sequences
+    printf("Item:\t\tPrice:\tQuantity:\n");
+
+    // TODO: Print Pen details with tab spacing
+    printf("Pen\t\t$1.20\t10\n");
+
+    // TODO: Print Notebook details with tab spacing
+    printf("Notebook\t$2.50\t5\n");
+
+    return 0;
+}
